949dd66ebba760187b912a8627fe4832a9d55730
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-12-05 08:50:51 -0800

    Update ArrayZoo.java

--- src/ArrayZoo.java
+++ src/ArrayZoo.java
@@ -5,23 +5,21 @@
 
 public class ArrayZoo {
     public static void main(String[] args) {
-        String animan [] = {"HoneyBudger", "Zorse", "Bangolin", "Bilby",
+        String[] animan = {"HoneyBudger", "Zorse", "Bangolin", "Bilby",
         "Hoatzin"};
-        for (int i = 0;i<=animan.length;i++){
+        for (int i = 0;i<animan.length;i++){
             System.out.println(animan[i]);
         }
-        System.out.println();
-        for (int i = 0;i<=animan.length;i++){
+        for (int i = 0;i<animan.length;i++){
             System.out.print(animan[i]);
         }
         System.out.println();
-        for (int i = 0;i<=animan.length;i++){
-            System.out.println(animan[i]+" ");
+        for (int i = 0;i<animan.length;i++){
+            System.out.print(animan[i]+" ");
         }
         System.out.println();
-        for (int i = 0;i<=animan.length;i++){
-            System.out.println(i+1+" "+animan[i]+"\n");
-            System.out.println(animan[i]);
+        for (int i = 0;i<animan.length;i++){
+            System.out.println(i+1+". "+animan[i]+"\n");
         }
     }
 }
