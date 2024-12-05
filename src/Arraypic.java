4687cb497d3855d523be3c732436e65094227c20
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-12-05 11:30:59 -0800

    Arraypic.java finish

---
+++ src/Arraypic.java
@@ -0,0 +1,31 @@
+public class Arraypic {
+    public static void main (String args[]){
+        String duck[] = {"Yellow","Yellow","Black","Yellow","Yellow",
+                "Yellow"};
+        String dice[] = {"Cow","Crocodile","Paw print","Wheel","Jellyfish",
+        "Sloth","Swan"};
+        String scarbble[] = {"O1","D2","S1","W4","R1","T1"};
+        String pvz[] = {"Sunflower","Pea shooter","Sunflower","Ice archer"};
+        String square[] = {"Red smile","White","Grey","Red","Red smile"};
+        String fruit[] = {"Acorn","Banana","Plums","Plums","Pears","Apple",
+                "Apple","Acorn"};
+        for (int i = 0;i<duck.length;i++){
+            System.out.println(duck[i]);
+        }
+        for (int i = 0;i<dice.length;i++){
+            System.out.println(dice[i]);
+        }
+        for (int i = 0;i<scarbble.length;i++){
+            System.out.println(scarbble[i]);
+        }
+        for (int i = 0;i<pvz.length;i++){
+            System.out.println(pvz[i]);
+        }
+        for (int i = 0;i<square.length;i++){
+            System.out.println(square[i]);
+        }
+        for (int i = 0;i<fruit.length;i++){
+            System.out.println(fruit[i]);
+        }
+    }
+}
