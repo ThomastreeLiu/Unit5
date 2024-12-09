c9f881967c32210863053276fd6fc70fb529a3f2
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-12-09 10:40:09 -0800

    Update Pumpkin.java

--- src/Pumpkin.java
+++ src/Pumpkin.java
@@ -2,13 +2,14 @@ public class Pumpkin {
     public static void main (String args[]){
         int weight[] = {222,235,335,410,380,577,262,430,211,279,408,292};
         for (int i = 0;i<weight.length;i++){
-            System.out.print(i+1+"."+weight[i]);
+            System.out.print(i+1+":"+weight[i]);
             System.out.print("  ");
         }
         int num = 0;
         for (int i = 0;i<weight.length;i++){
             num=num+weight[i];
         }
-        System.out.print("Total:"+num);
+        num=num/ weight.length+1;
+        System.out.println("Average:"+num);
     }
 }
